c2aa5917898cc97a0767ab4e4075fd79127a1f85
Ben Reynolds <breynolds.ctr@kr.af.mil>
2020-09-02 20:00:15 -0400

    Add firebase SDK and sample realtime database connection

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Pages/Home';
@@ -7,6 +9,32 @@ import { Recipes } from './Pages/Recipes';
 import store from './Redux/store';
 
 function App() {
+
+    useEffect(() => {
+        // Set the configuration for your app
+        // TODO: Replace with your project's config object
+        var config = {
+            apiKey: "apiKey",
+            authDomain: "projectId.firebaseapp.com",
+            databaseURL: "https://benreynolds-me.firebaseio.com/",
+            storageBucket: "bucket.appspot.com"
+        };
+        firebase.initializeApp(config);
+
+        // Get a reference to the database service
+        var database = firebase.database();
+
+        function writeUserData(userId, name, email) {
+            database.ref('users/' + userId).set({
+              username: name,
+              email: email,
+            });
+        }
+
+        writeUserData('foo', 'Ben', 'ben@example.com')
+
+    }, [ ]);
+
     return (
         <Provider store={store}>
             <Router>
