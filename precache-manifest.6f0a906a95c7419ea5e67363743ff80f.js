9245eaaab2534ffc7690203a17ba4729d0215b39
Ben Reynolds <breynolds.ctr@kr.af.mil>
2022-03-10 10:27:44 -0500

    Updates

---
+++ precache-manifest.6f0a906a95c7419ea5e67363743ff80f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd105c7d4c9d46525c85a3f912dde9c",
+    "url": "/benreynolds.me/index.html"
+  },
+  {
+    "revision": "65188a375600cf8a2bb2",
+    "url": "/benreynolds.me/static/css/main.5df324c9.chunk.css"
+  },
+  {
+    "revision": "2e17ea3b6c910cf1e9b4",
+    "url": "/benreynolds.me/static/js/2.f2b91d43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/benreynolds.me/static/js/2.f2b91d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65188a375600cf8a2bb2",
+    "url": "/benreynolds.me/static/js/main.4071d0a3.chunk.js"
+  },
+  {
+    "revision": "7c2953ce63c39275750a",
+    "url": "/benreynolds.me/static/js/runtime-main.682cb364.js"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/benreynolds.me/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "662a4f1472618eb2eb3b507d7459741f",
+    "url": "/benreynolds.me/static/media/instagram.662a4f14.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/benreynolds.me/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "c7c8d5ad36e8a8c8dbb97d9b9b560695",
+    "url": "/benreynolds.me/static/media/portrait.c7c8d5ad.jpg"
+  },
+  {
+    "revision": "aee4e77e7cf867e46ec2c2fff268e9ad",
+    "url": "/benreynolds.me/static/media/spotify.aee4e77e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a2e154634208557974f8611ac6ba89e.js"
+  "/benreynolds.me/precache-manifest.6f0a906a95c7419ea5e67363743ff80f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/benreynolds.me/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
